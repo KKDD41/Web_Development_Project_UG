6398fba4a8597a0930066be8eaa6f3329ec539ea
Ekaterina Dul <ekaterina_dul@epam.com>
2023-11-12 17:01:47 +0400

    Third partial slide on carousel

--- index.js
+++ index.js
@@ -14,22 +14,31 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let partialSlides = document.getElementsByClassName("myPartialSlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+  }
+  for (i = 0; i < partialSlides.length; i++) {
+    partialSlides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  console.log(" -> " + slideIndex.toString())
   if (slideIndex < 6) {
     slides[slideIndex-1].style.display = "inline-block";
     slides[slideIndex].style.display = "inline-block";
+    partialSlides[slideIndex].style.display = "inline-block";
   } else {
     slides[0].style.display = "inline-block";
     slides[1].style.display = "inline-block";
+    partialSlides[1].style.display = "inline-block";
   }
   dots[slideIndex-1].className += " active";
 }
