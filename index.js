45105afdf300ae5df650307974e99066bf482107
Ekaterina Dul <ekaterina_dul@epam.com>
2024-01-18 22:31:29 +0400

    Presentation + Comments

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-
 var mq = window.matchMedia("only screen and (max-width: 600px)")
-mq.addEventListener("change", function () {
-        showSlides(slideIndex);
-    }
-)
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// Function, that changes slideshow appearance based on medea query
+mq.addEventListener("change", function () { showSlides(slideIndex); })
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// Next / Previous Buttons controls
+function plusSlides(n) { showSlides(slideIndex += n); }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let partialSlides = document.getElementsByClassName("myPartialSlides");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < partialSlides.length; i++) {
-    partialSlides[i].style.display = "none";
-  }
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  // Hiding all full and partial slides
+  for (i = 0; i < slides.length; i++) { slides[i].style.display = "none"; }
+  for (i = 0; i < partialSlides.length; i++) { partialSlides[i].style.display = "none"; }
+
+  // Showing slides based on current index and medea query
   if (window.matchMedia("only screen and (max-width: 600px)").matches) {
-      console.log("Index: ->" + slideIndex.toString() + " length: -> " + slides.length)
-      if (slideIndex == 6 && n > 1) {
-        slideIndex = 1
-      } else if (slideIndex == 6 && n < 1) {
-        slideIndex = 5
-      }
+      if (slideIndex == 6 && n > 1) { slideIndex = 1 }
+      else if (slideIndex == 6 && n < 1) { slideIndex = 5 }
       slides[slideIndex - 1].style.display = "block"
   } else {
       if (slideIndex < 6) {
         slides[slideIndex-1].style.display = "inline-block";
         slides[slideIndex].style.display = "inline-block";
         partialSlides[slideIndex].style.display = "inline-block";
-      } else {
+      }
+      else {
         slides[0].style.display = "inline-block";
         slides[1].style.display = "inline-block";
         partialSlides[1].style.display = "inline-block";
@@ -72,19 +56,17 @@ function showServices() {
     document.getElementById("our-services").style.display = "block"
 }
 
+// Array of accordion options
 var acc = document.getElementsByClassName("service-option");
-console.log(acc.length)
 var i;
 
+// Function responsible for accordion content showing & hiding
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     console.log(panel.innerHTML)
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+    if (panel.style.maxHeight) { panel.style.maxHeight = null; }
+    else { panel.style.maxHeight = panel.scrollHeight + "px"; }
   });
 }
